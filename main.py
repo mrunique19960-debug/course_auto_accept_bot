cffc09ea2ae559ce08201a5e63064e3b4796039c
mrunique19960-debug <mrunique19960@gmail.com>
2025-11-20 11:35:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from pyrogram import Client, filters
 
-api_id = 30888567
+api_id = 30885867  # FIXED
 api_hash = "215559ea97000a748b444f3330f76659"
 bot_token = "7898349005:AAGnYnhIgODx3iIAC417yWDd_Fgr-y8F9G8"
 
 app = Client(
-    "auto_accept_bot",
+    "/tmp/auto_accept_bot",
     api_id=api_id,
     api_hash=api_hash,
     bot_token=bot_token
 )
 
-# Auto Approve Join Requests
 @app.on_chat_join_request()
 async def approve_request(client, req):
     try:
@@ -19,16 +18,14 @@ async def approve_request(client, req):
         print(f"Approved: {req.from_user.first_name}")
         await app.send_message(
             req.chat.id,
-            f"👋 Welcome {req.from_user.mention}!\nYou have been approved automatically."
+            f"👋 Welcome {req.from_user.mention}, you are approved automatically!"
         )
     except Exception as e:
         print("Error:", e)
 
-
-# /start command
 @app.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply("🔥 Auto Accept Bot is active and running!")
+async def start(_, message):
+    await message.reply("🔥 Auto Accept Bot is active!")
 
 print("Bot is running...")
 app.run()
